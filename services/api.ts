1793d92b651d93198043ff5154f23433aff2d9fc
Vitor Nunes <vitornunes200@gmail.com>
2026-01-09 16:31:34 -0300

    using git gish for api

--- services/api.ts
+++ services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://gist.githubusercontent.com/VitorN200/1382e78fce4070c1a09e58d06411e6c0/raw/bfdbcb19182f0bc2e586babe982f4af2a2cf90b8/db.json',
 });
 
 export const getDashboardData = async () => {
-    const response = await api.get('/dashboard');
+    const response = await api.get('/');
     return response.data;  
 };
---
+++ sw.js
@@ -0,0 +1,4 @@
+self.addEventListener('fetch', function(event) {
+  // Este é um service worker vazio (pass-through)
+  // Necessário apenas para o navegador habilitar o botão de instalar PWA
+});
